4f5342ecd48f96ba16daa359a6bfbf233f6f7d26
Ramazan Mamedov <mamedov.r9657@gmail.com>
2023-03-30 15:44:44 +0600

    Added SequentialLoadProfileWithDelay. Decorated SequentialLoadProfile and implemented delay logic using next execution DateTime.
    Added SequentialLoadProfileWithDelayTest.cs

---
+++ PressureApp.Tests/SequentialLoadProfileWithDelayTest.cs
@@ -0,0 +1,32 @@
+using PressureApp.LoadProfiles;
+
+namespace PressureApp.Tests;
+
+public class SequentialLoadProfileWithDelayTest
+{
+    [Fact]
+    public void WhenNextCanBeExecutedAsync_FirstCall_ReturnsCompletedTask()
+    {
+        var profile = new SequentialLoadProfileWithDelay(TimeSpan.FromMilliseconds(500));
+        var task = profile.WhenNextCanBeExecutedAsync(CancellationToken.None);
+        Assert.True(task.IsCompletedSuccessfully);
+    }
+    
+    [Fact]
+    public async Task WhenNextCanBeExecutedAsync_SecondCall_CompletesOnlyAfter_OnQueryExecutedAsync_AndDelay()
+    {
+        var profile = new SequentialLoadProfileWithDelay(TimeSpan.FromMilliseconds(10));
+        var _ = profile.WhenNextCanBeExecutedAsync(CancellationToken.None);
+        var task = profile.WhenNextCanBeExecutedAsync(CancellationToken.None);
+        Assert.False(task.IsCompleted);
+
+        await Task.Delay(15);
+        Assert.False(task.IsCompleted);
+
+        await profile.OnQueryExecutedAsync(CancellationToken.None);
+        Assert.False(task.IsCompleted);
+
+        await Task.Delay(15);
+        Assert.True(task.IsCompletedSuccessfully);
+    }
+}
--- PressureApp/LoadProfiles/TargetThroughputLoadProfile.cs
+++ PressureApp/LoadProfiles/TargetThroughputLoadProfile.cs
@@ -1,4 +1,3 @@
-using System.Runtime.InteropServices.JavaScript;
 using PressureApp.Interfaces;
 
 namespace PressureApp.LoadProfiles;
